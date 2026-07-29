02b3e07994c1b331287e897e95cdc6353b008bbf
Kyer Potts <kyerpotts@squidlabs.dev>
2026-07-29 11:02:57 +0800

    chore(nvim): update vulkanite colorscheme plugin def

--- .config/nvim/lua/plugins/vulkanite.lua
+++ .config/nvim/lua/plugins/vulkanite.lua
@@ -1,12 +1,14 @@
 return {
-  "kyerpotts/vulkanite.nvim",
-  name = "vulkanite.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    -- For initial visual QA, force every bundled integration on.
-    -- Remove this later to test package-manager auto-detection.
-    -- plugins = { all = true },
+  {
+    "kyerpotts/vulkanite.nvim",
+    name = "vulkanite.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- For initial visual QA, force every bundled integration on.
+      -- Remove this later to test package-manager auto-detection.
+      -- plugins = { all = true },
+    },
   },
   {
     "LazyVim/LazyVim",
