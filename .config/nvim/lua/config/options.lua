590ab6548b1b2b7f7b6dca79ea7b61080a0baeff
Kyer Potts <kyerpotts@squidlabs.dev>
2026-08-26 12:01:22 +0800

    perf(nvim): keep WSL pastes inside Neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,4 +1,8 @@
 require("config.remote_clipboard").setup()
+if vim.env.WSL_DISTRO_NAME ~= nil then
+  vim.opt.clipboard = ""
+end
+
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
